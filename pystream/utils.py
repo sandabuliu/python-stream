41d34f2f44b73eb8ea365d98dfba82bf6791ab33
tongbin <tongbin@haizhi.com>
2017-12-01 12:59:38 +0800

    Fix: Jsoncls

--- pystream/utils.py
+++ pystream/utils.py
@@ -8,18 +8,10 @@ __author__ = 'tong'
 
 
 class JSONCls(json.JSONEncoder):
-    def __init__(self, skipkeys=False, ensure_ascii=False, check_circular=True, allow_nan=True, sort_keys=False,
-                 indent=None, separators=None, encoding='', default=None):
-        def set_default(obj):
-            from sqlalchemy.engine.result import RowProxy
-            if isinstance(obj, RowProxy):
-                return list(obj)
-            raise TypeError('%s(%s) is not JSON serializable' % (obj, type(obj)))
-
-        super(JSONCls, self).__init__(skipkeys, ensure_ascii, check_circular, allow_nan, sort_keys, indent, separators,
-                                      encoding, default or set_default)
-
     def default(self, obj):
+        from sqlalchemy.engine.result import RowProxy
+        if isinstance(obj, RowProxy):
+            return list(obj)
         if isinstance(obj, datetime):
             return '%04d-%02d-%02d %02d:%02d:%02d' % (obj.year, obj.month, obj.day, obj.hour, obj.minute, obj.second)
         if isinstance(obj, date):
